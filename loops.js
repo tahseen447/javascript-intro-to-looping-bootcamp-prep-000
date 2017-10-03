94a1966ad3a79954f1216efbcb7f57682db7f5a4
tahseen447 <tahseenfatima1986@gmail.com>
2017-10-03 17:01:30 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,44 @@
+function mayBeTrue()
+{
+  return Math.random() >= 0.5
+}
+
+function forLoop(array)
+{
+  var len = array.len
+
+  for(var i = 0; i<=25; i++)
+  {
+    if( i === 1)
+    {
+      array[len+i]= "I am 1 strange loop."
+    }
+    else
+     {
+      array[len+i]="I am ${i} strange loops."
+    }
+  }
+  return array
+}
+
+function whileLoop(n)
+{
+  while(n>0)
+  {
+    console.log("n")
+    --n
+  }
+  return "done"
+}
+
+function doWhileLoop(array)
+{
+  do
+  {
+    array.pop()
+  }
+  while(mayBeTrue() || array.length>0)
+  {
+    array.pop()
+  }
+}
